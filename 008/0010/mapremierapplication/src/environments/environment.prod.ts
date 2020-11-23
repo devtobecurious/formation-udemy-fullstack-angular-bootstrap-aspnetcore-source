ce24f553cef88d69a932d87ca4ba855216aba108
Eavn BOISSONNOT <evan@boissonnot.fr>
2020-11-23 17:13:55 +0100

    (param): publish on web

--- 008/0010/mapremierapplication/src/environments/environment.prod.ts
+++ 008/0010/mapremierapplication/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apis: {
     selfies: {
-      url: '<<a definir>>'
+      url: 'https://78492da9-ed57-4e28-82bc-1ac7931aeb27.mock.pstmn.io/selfie'
     }
   }
 };
